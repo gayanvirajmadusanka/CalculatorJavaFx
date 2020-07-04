56ec74397387189ab90540024df2f09f8ffcbadd
Gayan Madusanka <gayan.virajs@gmail.com>
2020-07-04 10:36:31 +0530

    Commit test 2

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,6 +1,69 @@
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 
 public class MainFormController {
 
     public JFXTextField txt;
+    public JFXButton btnAc;
+    public JFXButton btnMultiple;
+    public JFXButton btnDevide;
+    public JFXButton btnPercantage;
+    public JFXButton btn1;
+    public JFXButton btn2;
+    public JFXButton btn3;
+    public JFXButton btn4;
+    public JFXButton btn5;
+    public JFXButton btn6;
+    public JFXButton btn7;
+    public JFXButton btn8;
+    public JFXButton btn9;
+    public JFXButton btn0;
+    public JFXButton btnAdd;
+    public JFXButton btnEqual;
+
+    public void btnAcOnActon(ActionEvent actionEvent) {
+    }
+
+    public void btnMultipleOnActiion(ActionEvent actionEvent) {
+    }
+
+    public void btnDevideOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPercantageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn1OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn2OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn3OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn4OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn5OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn6OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn7OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn8OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn9OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn0OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
 }
